84883a23de5b6e9895fcf4a0f58a26043d422908
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-09-01 13:41:00 -0300

    feat: ensure DbAddAccountRepository call AddAccount with correct values

--- src/data/usecases/account/add-account/db-add-account.ts
+++ src/data/usecases/account/add-account/db-add-account.ts
@@ -1,15 +1,17 @@
 import { AccountModel } from '../../../../domain/models/account';
 import { AddAccount, AddAccountModel } from '../../../../domain/usecases/add-account';
+import { AddAccountRepository } from '../../../protocols/db/account/add-account-repository';
 import { LoadAccountByEmailRepository } from '../../../protocols/db/account/load-account-by-email-repository';
 import { ValidateAddAccountKeyRepository } from '../../../protocols/db/keys/validate-add-account-key-repository';
 
 export class DbAddAccountRepository implements AddAccount {
   constructor(
     private readonly loadAccount: LoadAccountByEmailRepository,
-    private readonly validateKey: ValidateAddAccountKeyRepository
+    private readonly validateKey: ValidateAddAccountKeyRepository,
+    private readonly account: AddAccountRepository
     ) {}
   async add (account: AddAccountModel): Promise<AccountModel | null> {
-    const { email, privateKey } = account
+    const { email, privateKey, username, password, passwordConfirmation } = account
     const loadAccount = await this.loadAccount.loadByEmail(email)
     if(!loadAccount) {
       return null
@@ -18,6 +20,7 @@ export class DbAddAccountRepository implements AddAccount {
     if(!validate) {
       return null
     }
+    await this.account.addAccount({ username, email, password, privateKey, passwordConfirmation})
     return {
       username: 'any_username',
       email: 'any_email@mail.com',
