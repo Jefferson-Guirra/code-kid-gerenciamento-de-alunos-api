709f2bc7ccf54ffe3ee94f13ff184c8e27b2fcfe
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-09-10 23:26:17 -0300

    feat: ensure DbAddAccountRepository call Hasher with correct password

--- src/data/usecases/account/add-account/db-add-account.ts
+++ src/data/usecases/account/add-account/db-add-account.ts
@@ -1,5 +1,6 @@
 import { AccountModel } from '../../../../domain/models/account';
 import { AddAccount, AddAccountModel } from '../../../../domain/usecases/add-account';
+import { Hasher } from '../../../protocols/criptography/hasher';
 import { AddAccountRepository } from '../../../protocols/db/account/add-account-repository';
 import { LoadAccountByEmailRepository } from '../../../protocols/db/account/load-account-by-email-repository';
 import { ValidateAddAccountKeyRepository } from '../../../protocols/db/keys/validate-add-account-key-repository';
@@ -8,7 +9,8 @@ export class DbAddAccountRepository implements AddAccount {
   constructor(
     private readonly loadAccount: LoadAccountByEmailRepository,
     private readonly validateKey: ValidateAddAccountKeyRepository,
-    private readonly account: AddAccountRepository
+    private readonly account: AddAccountRepository,
+    private readonly hasher : Hasher
     ) {}
   async add (account: AddAccountModel): Promise<AccountModel | null> {
     const { email, privateKey, username, password, passwordConfirmation } = account
@@ -20,6 +22,7 @@ export class DbAddAccountRepository implements AddAccount {
     if(!validate) {
       return null
     }
+    await this.hasher.hash(password)
     return await this.account.addAccount({ username, email, password, privateKey, passwordConfirmation})
   }
 }
