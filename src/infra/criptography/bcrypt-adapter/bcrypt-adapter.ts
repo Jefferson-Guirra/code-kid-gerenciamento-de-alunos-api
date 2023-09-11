d680cf76ada9dcd73b030361ef9ebbbdcde986cf
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-09-11 20:32:43 -0300

    feat: ensure BcryptAdapter call Hash with correct values

---
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -0,0 +1,10 @@
+import { Hasher } from '../../../data/protocols/criptography/hasher';
+import bcrypt from 'bcrypt'
+
+export class BcryptAdapter implements Hasher {
+  constructor(  private readonly salt: number ) {}
+  async hash (value: string): Promise<string> {
+  
+    return await bcrypt.hash(value, this.salt)
+  }
+}
