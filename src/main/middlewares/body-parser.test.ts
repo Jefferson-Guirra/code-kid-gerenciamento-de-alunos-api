d97314ee542be02d7ab05b9d6ca88836ea2c5d23
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-09-10 22:51:49 -0300

    test: ensure soutes parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest'
+import app from '../config/app'
+describe('bodyParser', () => { 
+  test('should parse body as json', async () => { 
+     app.post('/test_body_parser', (req, res) => {
+      return res.send(req.body)
+     })
+     await request(app).post('/test_body_parser').send({name: 'jefferson'}).expect({name: 'jefferson'})
+   })
+
+})
