e4a78a2701f9ebd21df6689afcc40f5aaa5f01c7
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-09-11 21:00:08 -0300

    feat: ensure CompareFields return undefined on succeeds

---
+++ src/presentation/helpers/validators/compare-fields-validator.ts
@@ -0,0 +1,16 @@
+import { InvalidParamsError } from '../../errors/invalid-params-error'
+import { HttpRequest } from '../../protocols/http'
+import { Validation } from '../../protocols/validation'
+
+export class CompareFields implements Validation {
+  constructor( 
+    private readonly field: string, 
+    private readonly compareFields: string) {}
+
+  validation(httpRequest: HttpRequest): Error | undefined {
+    const  body = httpRequest.body
+    if(body[this.field] !== body[this.compareFields]){
+      return new InvalidParamsError(this.field)
+    }
+  }
+}
