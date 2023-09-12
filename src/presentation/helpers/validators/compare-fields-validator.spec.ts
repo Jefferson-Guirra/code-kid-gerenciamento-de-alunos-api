532d0dafb585a6daf1c4a41ac06ef14cae6659fe
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-09-11 21:15:23 -0300

    test: ensure CompareFields return InvalidParamsError if validation fails

--- src/presentation/helpers/validators/compare-fields-validator.spec.ts
+++ src/presentation/helpers/validators/compare-fields-validator.spec.ts
@@ -1,17 +1,29 @@
+import { InvalidParamsError } from '../../errors/invalid-params-error'
 import { HttpRequest } from '../../protocols/http'
 import { CompareFields } from './compare-fields-validator'
 
-const makeFakeRequest = ():HttpRequest => ({
-  body: {
-    password: 'any_password',
-    passwordConfirmation: 'any_password'
+const makeFakeRequest = (value?: string):HttpRequest => {
+  const body ={
+    field: 'any_password',
+    fieldToCompare: 'any_password'
   }
-})
-const makeSut= (): CompareFields => new CompareFields('password', 'passwordConfirmation')
+
+  if(value) body.fieldToCompare = value
+  return { body }
+  
+
+}
+const makeSut= (): CompareFields => new CompareFields('field', 'fieldToCompare')
 describe('CompareFields', () => { 
   test('should return undefined on succeeds', () => {
     const sut = makeSut()
     const response = sut.validation(makeFakeRequest())
     expect(response).toBeFalsy()
+  })
+
+  test('should return  on succeeds', () => {
+    const sut = makeSut()
+    const response = sut.validation(makeFakeRequest('random_value'))
+    expect(response).toEqual(new InvalidParamsError('fieldToCompare'))
   })
 })
