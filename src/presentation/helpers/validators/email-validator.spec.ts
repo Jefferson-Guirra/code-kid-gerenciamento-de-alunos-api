9d73e1c716acb9761ec1642628b64ccfbd0b3cd5
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-09-12 20:56:45 -0300

    test: ensure EmailValidation call EmailValidator with correct email

---
+++ src/presentation/helpers/validators/email-validator.spec.ts
@@ -0,0 +1,43 @@
+import { EmailValidator } from '../../protocols/email-validator'
+import { HttpRequest } from '../../protocols/http'
+import { EmailValidation } from './email-validator'
+
+const makeFakeRequest = (): HttpRequest =>  ({
+  body: {
+    username: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password'
+  }
+})
+
+const makeEmailValidatorStub = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(value: string):  boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+interface SutTypes {
+  emailValidatorStub: EmailValidator,
+  sut: EmailValidation
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeEmailValidatorStub()
+  const sut = new EmailValidation('email', emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
+}
+describe('EmailValidation', () => { 
+  test('should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const validatorSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    sut.validation(makeFakeRequest())
+    expect(validatorSpy).toBeCalledWith('any_email@mail.com')
+
+  })
+
+})
