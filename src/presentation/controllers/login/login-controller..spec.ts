ee318732d453eb6555d998c55a63299e60427994
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-09-21 21:44:03 -0300

    test: ensure LoginController return 500 if LoadAccunt fails

--- src/presentation/controllers/login/login-controller..spec.ts
+++ src/presentation/controllers/login/login-controller..spec.ts
@@ -1,6 +1,6 @@
 import { LoadAccountByEmailRepository } from '../../../data/protocols/db/account/load-account-by-email-repository'
 import { AccountModel } from '../../../domain/models/account'
-import { unauthorized } from '../../helpers/http/http'
+import { serverError, unauthorized } from '../../helpers/http/http'
 import { HttpRequest } from '../../protocols/http'
 import { LoginController } from './login-controller'
 
@@ -53,5 +53,12 @@ describe('LoginController', () => {
     jest.spyOn(loadAccountStub, 'loadByEmail').mockReturnValueOnce(Promise.resolve(null))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(unauthorized())
+  })
+  test('should return throw if loadAccount return throw', async () => { 
+     const { sut, loadAccountStub }  = makeSut()
+     jest.spyOn(loadAccountStub, 'loadByEmail').mockReturnValueOnce(Promise.reject( new Error('')))
+     const response = await sut.handle(makeFakeRequest())
+     expect(response).toEqual(serverError(new Error('')))
+     
   })
 })
