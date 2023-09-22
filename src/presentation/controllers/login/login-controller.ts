ed85d2a2cfa01539acee2811e8fb70df974899fa
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-09-21 21:43:09 -0300

    feat: ensure LoginController return 500 if LoadAccunt fails

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,15 +1,19 @@
 import { LoadAccountByEmailRepository } from '../../../data/protocols/db/account/load-account-by-email-repository';
-import { ok, unauthorized } from '../../helpers/http/http';
+import { ok, serverError, unauthorized } from '../../helpers/http/http';
 import { Controller } from '../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../protocols/http';
 
 export class LoginController  implements Controller {
   constructor( private readonly loadAccount: LoadAccountByEmailRepository){}
   async handle (request: HttpRequest): Promise<HttpResponse>{
-    const { email, password } = request.body
-    const account = await this.loadAccount.loadByEmail(email)
-    if(!account) {
-      return unauthorized()
+    try{
+      const { email, password } = request.body
+      const account = await this.loadAccount.loadByEmail(email)
+      if(!account) {
+        return unauthorized()
+      }
+    } catch(err) {
+      return serverError(err as Error)
     }
     return ok('success')
   }
