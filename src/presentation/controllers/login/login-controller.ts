cf30e349928d05ac87cecf525e6163438ad02b6c
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-09-21 21:33:38 -0300

    feat: ensure LoginController return 401 if LoadAccunt return null

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,5 +1,5 @@
 import { LoadAccountByEmailRepository } from '../../../data/protocols/db/account/load-account-by-email-repository';
-import { ok } from '../../helpers/http/http';
+import { ok, unauthorized } from '../../helpers/http/http';
 import { Controller } from '../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../protocols/http';
 
@@ -7,7 +7,10 @@ export class LoginController  implements Controller {
   constructor( private readonly loadAccount: LoadAccountByEmailRepository){}
   async handle (request: HttpRequest): Promise<HttpResponse>{
     const { email, password } = request.body
-    await this.loadAccount.loadByEmail(email)
+    const account = await this.loadAccount.loadByEmail(email)
+    if(!account) {
+      return unauthorized()
+    }
     return ok('success')
   }
 }
