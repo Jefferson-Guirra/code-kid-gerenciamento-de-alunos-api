00c458a4dfb324742c0b03f2951076fd2f59977e
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-09-21 21:54:43 -0300

    feat: ensure LoginController call HashCompare with correct values

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,10 +1,15 @@
+import { HashCompare } from '../../../data/protocols/criptography/hash-compare';
 import { LoadAccountByEmailRepository } from '../../../data/protocols/db/account/load-account-by-email-repository';
 import { ok, serverError, unauthorized } from '../../helpers/http/http';
 import { Controller } from '../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../protocols/http';
 
 export class LoginController  implements Controller {
-  constructor( private readonly loadAccount: LoadAccountByEmailRepository){}
+  constructor( 
+    private readonly loadAccount: LoadAccountByEmailRepository,
+    private readonly hashCompare: HashCompare
+  ){}
+
   async handle (request: HttpRequest): Promise<HttpResponse>{
     try{
       const { email, password } = request.body
@@ -12,6 +17,7 @@ export class LoginController  implements Controller {
       if(!account) {
         return unauthorized()
       }
+      const isValid = await this.hashCompare.compare(password, account.password)
     } catch(err) {
       return serverError(err as Error)
     }
