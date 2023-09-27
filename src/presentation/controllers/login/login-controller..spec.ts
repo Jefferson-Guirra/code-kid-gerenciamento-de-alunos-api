d6ac5f056ed6a8fb5c6a826c09aa1519545e0494
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-09-26 23:05:49 -0300

    test: ensure LoginController return throw if HashCompare fails

--- src/presentation/controllers/login/login-controller..spec.ts
+++ src/presentation/controllers/login/login-controller..spec.ts
@@ -101,6 +101,14 @@ describe('LoginController', () => {
     expect(response).toEqual(unauthorized())
    })
 
+
+  test('should return 500 if HashCompare fails', async () => { 
+    const { sut, hashCompareStub }  = makeSut()
+    jest.spyOn(hashCompareStub, 'compare').mockReturnValueOnce(Promise.reject( new Error('')))
+    const response = await sut.handle(makeFakeRequest())
+    expect(response).toEqual(serverError(new Error('')))
+ })
+
    test('should call Encrypter with correct values', async() => { 
     const { sut, encrypterStub } = makeSut()
     const hashSpy = jest.spyOn(encrypterStub, 'encrypt')
