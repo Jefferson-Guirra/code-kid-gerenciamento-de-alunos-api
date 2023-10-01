932f4ed482183af8cc5e4c6b88ab7cb14f96b1d7
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-10-01 19:28:38 -0300

    feat:  ensure LoginController call Validation with correct value

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -5,14 +5,17 @@ import { Authentication } from '../../../domain/usecases/account/authentication'
 import { ok, serverError, unauthorized } from '../../helpers/http/http';
 import { Controller } from '../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../protocols/http';
+import { Validation } from '../../protocols/validation';
 
 export class LoginController  implements Controller {
   constructor( 
+    private readonly validation: Validation,
     private readonly authentication: Authentication
 
   ){}
 
   async handle (request: HttpRequest): Promise<HttpResponse>{
+    this.validation.validation(request)
     try{
       const { email, password } = request.body
       const account =  await this.authentication.auth(email, password)
