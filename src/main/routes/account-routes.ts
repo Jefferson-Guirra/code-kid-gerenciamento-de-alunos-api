1d694a6a92a904340e1471a60c7c865e3cda1357
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-10-01 20:23:06 -0300

    feat: create Login route

--- src/main/routes/account-routes.ts
+++ src/main/routes/account-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { adapterRouter } from '../adapters/express/express-adapter-routes';
 import { makeSignupController } from '../factories/account/signup/signup-controller-factory';
+import { makeLoginControllerFactory } from '../factories/account/login/login-controller-factory';
 
 const accountRoute = (router: Router) => {
   router.post('/signup',adapterRouter(makeSignupController()))
+  router.post('/login', adapterRouter(makeLoginControllerFactory()))
 }
 
 export default accountRoute
