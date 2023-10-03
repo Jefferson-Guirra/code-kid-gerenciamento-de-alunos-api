b53378fada6a48885638764fd1893ce9c76fe964
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-10-02 22:27:07 -0300

    test: ensure LogoutController return 200 on succeeds

--- src/presentation/controllers/logout/logout-controller.spec.ts
+++ src/presentation/controllers/logout/logout-controller.spec.ts
@@ -1,6 +1,6 @@
 import { AccountLogout } from '../../../domain/usecases/account/logout-account';
 import { MissingParamsError } from '../../errors/missing-params-error';
-import { badRequest, serverError, unauthorized } from '../../helpers/http/http';
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http';
 import { HttpRequest } from '../../protocols/http';
 import { Validation } from '../../protocols/validation';
 import { LogoutController } from './logout-controller';
@@ -80,6 +80,12 @@ describe('LogoutController', () => {
     jest.spyOn(DbAccountLogout, 'logout').mockReturnValueOnce(Promise.reject(new Error('')))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(serverError(new Error('')))
+  })
+
+  test('should return 200 on succeeds', async () => { 
+    const { sut } = makeSut()
+    const response = await sut.handle(makeFakeRequest())
+    expect(response).toEqual(ok({ message: 'logout success'}))
   })
 
 })
