dc964e1b658b2cf7aa87e71f04f1d69230c58988
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-10-02 22:03:39 -0300

    test: ensure LogoutController return 500 if AccountLogout fails

--- src/presentation/controllers/logout/logout-controller.spec.ts
+++ src/presentation/controllers/logout/logout-controller.spec.ts
@@ -1,6 +1,6 @@
 import { AccountLogout } from '../../../domain/usecases/account/logout-account';
 import { MissingParamsError } from '../../errors/missing-params-error';
-import { badRequest, unauthorized } from '../../helpers/http/http';
+import { badRequest, serverError, unauthorized } from '../../helpers/http/http';
 import { HttpRequest } from '../../protocols/http';
 import { Validation } from '../../protocols/validation';
 import { LogoutController } from './logout-controller';
@@ -73,6 +73,13 @@ describe('LogoutController', () => {
     jest.spyOn(DbAccountLogout, 'logout').mockReturnValueOnce(Promise.resolve(undefined))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(unauthorized())
+  })
+
+  test('should return 500 if AccountLogout fails', async () => { 
+    const { sut, DbAccountLogout } = makeSut()
+    jest.spyOn(DbAccountLogout, 'logout').mockReturnValueOnce(Promise.reject(new Error('')))
+    const response = await sut.handle(makeFakeRequest())
+    expect(response).toEqual(serverError(new Error('')))
   })
 
 })
