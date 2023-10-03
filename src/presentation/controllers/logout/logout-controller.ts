cbe718cafa05eec09f6c1342bb05953969086c5d
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-10-02 21:45:01 -0300

    feat: ensure LogoutController call AccountLogout with correct access_token

--- src/presentation/controllers/logout/logout-controller.ts
+++ src/presentation/controllers/logout/logout-controller.ts
@@ -1,15 +1,21 @@
+import { AccountLogout } from '../../../domain/usecases/account/logout-account';
 import { badRequest, ok } from '../../helpers/http/http';
 import { Controller } from '../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../protocols/http';
 import { Validation } from '../../protocols/validation';
 
 export class LogoutController implements Controller {
-  constructor( private readonly validator: Validation) {}
+  constructor( 
+    private readonly validator: Validation,
+    private readonly accountLogout: AccountLogout
+  ) {}
   async handle (request: HttpRequest): Promise<HttpResponse> {
     const error = this.validator.validation(request)
     if(error) {
       return badRequest(error)
     }
+    const { accessToken } = request.body
+    await this.accountLogout.logout(accessToken)
      return ok(' success')
   }
 }
