6026d84b3c54045a2e49f5be74b969fc6db043be
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-10-25 21:35:52 -0300

    feat: create AccountLoginModel interface

--- src/data/protocols/db/account/load-account-by-access-token-repository.ts
+++ src/data/protocols/db/account/load-account-by-access-token-repository.ts
@@ -1,5 +1,9 @@
 import { AccountModel } from '../../../../domain/models/account';
 
+export interface AccountLoginModel extends AccountModel {
+  accessToken: string,
+}
+
 export interface LoadAccountByAccessTokenRepository {
-  loadByAccessToken: (accessToken: string ) => Promise<AccountModel | null>
+  loadByAccessToken: (accessToken: string ) => Promise<AccountLoginModel | null>
 }
