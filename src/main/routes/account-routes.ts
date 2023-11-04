5e5fcfe0c2b9253bdb4efcb11fe24f29615875dd
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-11-03 21:10:36 -0300

    feat: create logout rout to logout user

--- src/main/routes/account-routes.ts
+++ src/main/routes/account-routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import { adapterRouter } from '../adapters/express/express-adapter-routes';
 import { makeSignupController } from '../factories/account/signup/signup-controller-factory';
 import { makeLoginControllerFactory } from '../factories/account/login/login-controller-factory';
+import { makeLogoutControllerFactory } from '../factories/account/logout/logout-controller-factory';
 
 const accountRoute = (router: Router) => {
   router.post('/signup',adapterRouter(makeSignupController()))
   router.post('/login', adapterRouter(makeLoginControllerFactory()))
+  router.delete('/logout', adapterRouter(makeLogoutControllerFactory()))
 }
 
 export default accountRoute
