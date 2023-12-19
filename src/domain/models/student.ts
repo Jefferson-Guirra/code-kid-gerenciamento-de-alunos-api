3646e0a9cfdfb3ceb2ba5964ec57dc8a83ccf43f
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-12-19 20:58:17 -0300

    feat: ensure AddStudentController call Validation with correct values

---
+++ src/domain/models/student.ts
@@ -0,0 +1,7 @@
+export interface Student {
+  name: string,
+  age: number,
+  father: string
+  mother:string,
+  phone: number
+}
---
+++ src/presentation/controllers/student/add-student-controller.ts
@@ -0,0 +1,18 @@
+import { badRequest, ok } from '../../helpers/http/http';
+import { Controller } from '../../protocols/controller';
+import { HttpRequest, HttpResponse } from '../../protocols/http';
+import { Validation } from '../../protocols/validation';
+
+export class AddStudentController implements Controller {
+  constructor(
+    private readonly validator: Validation
+  ){}
+   async handle(request: HttpRequest): Promise<HttpResponse> {
+    const error = this.validator.validation(request)
+    if (error) {
+      return badRequest(error)
+    }
+    return ok('success')
+
+  }
+}
