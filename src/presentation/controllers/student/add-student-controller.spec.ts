939080514b9874bdd99a363d343933574c7da9fb
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-12-20 21:11:54 -0300

    test: ensure AddStudentController return 500 if AddStudent return throw

--- src/presentation/controllers/student/add-student-controller.spec.ts
+++ src/presentation/controllers/student/add-student-controller.spec.ts
@@ -1,7 +1,7 @@
 import { Student } from '../../../domain/models/student'
 import { AddStudent, AddStudentModel } from '../../../domain/usecases/student/add-student'
 import { MissingParamsError } from '../../errors/missing-params-error'
-import { badRequest, unauthorized } from '../../helpers/http/http'
+import { badRequest, serverError, unauthorized } from '../../helpers/http/http'
 import { HttpRequest } from '../../protocols/http'
 import { Validation } from '../../protocols/validation'
 import { AddStudentController } from './add-student-controller'
@@ -93,7 +93,12 @@ describe('AddStudentController', () => {
     jest.spyOn(addStudentStub, 'add').mockReturnValueOnce(Promise.resolve(null))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(unauthorized())
-
+  })
+  test('should return 500 if AddStudent return throw', async () => { 
+    const { sut, addStudentStub } = makeSut()
+    jest.spyOn(addStudentStub, 'add').mockReturnValueOnce(Promise.reject(new Error('')))
+    const response = await sut.handle(makeFakeRequest())
+    expect(response).toEqual(serverError(new Error('')))
   })
 
 })
