903bdc529320bb35631a58a850fe65241c0661fd
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-12-24 18:58:56 -0300

    test: ensure DbAddStudent call LoadStudentByName with correct name

---
+++ src/data/usecases/student/add-student/db-add-student.spec.ts
@@ -0,0 +1,50 @@
+import { Student } from '../../../../domain/models/student';
+import { AddStudentModel } from '../../../../domain/usecases/student/add-student';
+import { LoadStudentByName } from '../../../protocols/db/student/load-student-by-name';
+import { DbAddStudent } from './db-add-student';
+
+const makeFakeRequest = (): Student => ({
+    name: 'any_name',
+    age: 0,
+    father: 'any_father',
+    mother: 'any_mother',
+    phone: 0,
+    course: ['any_course'],
+    payment: 'yes',
+    registration: 'active',
+    date_payment: 'any_date'
+})
+
+const makeLoadStudentStub = (): LoadStudentByName => {
+  class LoadStudentStub implements LoadStudentByName {
+    async loadByName (name: string): Promise<AddStudentModel | null> {
+      return await Promise.resolve(null)
+    }
+  }
+  return new LoadStudentStub()
+}
+
+interface SuTypes {
+  loadStudentStub: LoadStudentByName
+  sut: DbAddStudent
+}
+
+const makeSut = (): SuTypes => {
+  const loadStudentStub = makeLoadStudentStub()
+  const sut = new DbAddStudent(loadStudentStub)
+
+  return {
+    loadStudentStub,
+    sut
+  }
+}
+
+describe('DbAddStudent', () => { 
+  test('should call LoadStudent with correct name', async () => {
+    const { sut, loadStudentStub } = makeSut()
+    const loadSpy = jest.spyOn(loadStudentStub, 'loadByName')
+    await sut.add(makeFakeRequest())
+    expect(loadSpy).toBeCalledWith('any_name')
+  })
+
+})
