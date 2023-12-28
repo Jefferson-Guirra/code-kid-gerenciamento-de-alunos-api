485e838c14e4f8703ec9bda447f388836613a82e
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-12-28 18:32:24 -0300

    feat: ensure StudentMongoRepository return student if LoadStudentByName success

--- src/infra/db/student/student-mongo-repository.ts
+++ src/infra/db/student/student-mongo-repository.ts
@@ -1,14 +1,25 @@
 import { AddStudentRepository } from '../../../data/protocols/db/student/add-student-repository';
+import { LoadStudentByNameRepository } from '../../../data/protocols/db/student/load-student-by-name-repository';
 import { Student } from '../../../domain/models/student';
 import { AddStudentModel } from '../../../domain/usecases/student/add-student';
 import { MongoHelper } from '../helpers/mongo-helper';
 
-export class StudentMongoRepository implements AddStudentRepository {
+export class StudentMongoRepository implements 
+AddStudentRepository,
+LoadStudentByNameRepository {
   async add(student: Student): Promise<AddStudentModel | null> {
     const studentCollections = await MongoHelper.getCollection('students')
     const result = await studentCollections.insertOne(student)
     const findStudent = await studentCollections.findOne({ _id: result.insertedId})
     return findStudent && MongoHelper.Map(findStudent)
+  }
+
+  async loadByName(name: string): Promise<AddStudentModel | null> {
+    const studentsCollection = await MongoHelper.getCollection('students')
+    const student = await studentsCollection.findOne({ name })
+    return student && MongoHelper.Map(student)
 
   }
+
+
 }
