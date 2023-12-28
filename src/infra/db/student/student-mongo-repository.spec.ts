9b52dab37740076cdc13d3343236d07ad080c61e
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-12-27 21:17:07 -0300

    test: ensure StudentMongoRepository return a student if add succeeds

---
+++ src/infra/db/student/student-mongo-repository.spec.ts
@@ -0,0 +1,52 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { StudentMongoRepository } from './student-mongo-repository'
+import { Student } from '../../../domain/models/student'
+
+const makeFakeRequest = (): Student => ({
+  name: 'any_name',
+  age: 0,
+  father: 'any_father',
+  mother: 'any_mother',
+  phone: 0,
+  course: ['any_course'],
+  payment: 'yes',
+  registration: 'active',
+  date_payment: 'any_date'
+})
+
+let studentsCollection: Collection
+const makeSut = () => new StudentMongoRepository()
+describe('StudentMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+  beforeEach(async () => {
+    studentsCollection = await MongoHelper.getCollection('students')
+    await studentsCollection.deleteMany({})
+  })
+
+  afterAll(async () =>{
+    await MongoHelper.disconnect()
+  })
+
+  test('should return student if add succeeds ', async () => {
+    const sut = makeSut()
+    let count = await studentsCollection.countDocuments()
+    expect(count).toBe(0)
+    const student = await sut.add(makeFakeRequest())
+    count = await studentsCollection.countDocuments()
+    expect(count).toBe(1)
+    expect(student?.name).toEqual('any_name')
+    expect(student?.age).toBe(0)
+    expect(student?.id).toBeTruthy()
+    expect(student?.date_payment).toEqual('any_date')
+    expect(student?.father).toEqual('any_father')
+    expect(student?.mother).toEqual('any_mother')
+    expect(student?.registration).toEqual('active')
+    expect(student?.course).toEqual(['any_course'])
+    expect(student?.name).toEqual('any_name')
+    expect(student?.phone).toEqual(0)
+
+  })
+})
