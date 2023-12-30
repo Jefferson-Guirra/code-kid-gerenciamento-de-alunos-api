31488801c60d5153bbc1f8a595080a0369eca480
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-12-29 22:11:16 -0300

    feat: ensure RemoveStudentController return 400 if Validation return error

--- src/presentation/controllers/student/remove/remove-student-controller.ts
+++ src/presentation/controllers/student/remove/remove-student-controller.ts
@@ -1,4 +1,4 @@
-import { ok } from '../../../helpers/http/http';
+import { badRequest, ok } from '../../../helpers/http/http';
 import { Controller } from '../../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../../protocols/http';
 import { Validation } from '../../../protocols/validation';
@@ -6,7 +6,10 @@ import { Validation } from '../../../protocols/validation';
 export class RemoveStudentController implements Controller {
   constructor(private readonly validator: Validation) {}
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    this.validator.validation(request)
+    const error = this.validator.validation(request)
+    if(error) {
+      return badRequest(error)
+    }
     return ok('success')
   }
 }
