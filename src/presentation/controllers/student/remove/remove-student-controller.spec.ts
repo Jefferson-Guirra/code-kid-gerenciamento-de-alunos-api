9ccf179e5b63b029f4085591f4654850ba82010a
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-12-29 22:41:39 -0300

    test: ensure RemoveStudentController return 200 on succeeds

--- src/presentation/controllers/student/remove/remove-student-controller.spec.ts
+++ src/presentation/controllers/student/remove/remove-student-controller.spec.ts
@@ -1,6 +1,6 @@
 import { RemoveStudent } from '../../../../domain/usecases/student/remove-student';
 import { MissingParamsError } from '../../../errors/missing-params-error';
-import { badRequest, serverError, unauthorized } from '../../../helpers/http/http';
+import { badRequest, ok, serverError, unauthorized } from '../../../helpers/http/http';
 import { HttpRequest } from '../../../protocols/http';
 import { Validation } from '../../../protocols/validation';
 import { RemoveStudentController } from './remove-student-controller';
@@ -80,5 +80,11 @@ describe('RemoveStudentController', () => {
     jest.spyOn(removeStudentStub, 'remove').mockReturnValueOnce(Promise.reject(new Error('')))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(serverError(new Error('')))
+  })
+
+  test('should return 200 on succeeds', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle(makeFakeRequest())
+    expect(response).toEqual(ok('removed'))
   })
 })
