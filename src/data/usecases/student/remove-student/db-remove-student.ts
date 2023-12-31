e9de9d3baea9547cb9b88445d9f3cca3180a5d0a
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-12-31 19:25:37 -0300

    feat: ensure DbRemoveStudent return null if LoadStudentById return null

--- src/data/usecases/student/remove-student/db-remove-student.ts
+++ src/data/usecases/student/remove-student/db-remove-student.ts
@@ -4,7 +4,10 @@ import { LoadStudentByIdRepository } from '../../../protocols/db/student/load-st
 export class DbRemoveStudent implements RemoveStudent {
   constructor(private readonly loadStudent: LoadStudentByIdRepository) {}
   async remove(id: string): Promise<'removed' | null> {
-    await this.loadStudent.loadById(id)
-    return null
+    const student = await this.loadStudent.loadById(id)
+    if(!student) {
+      return null
+    }
+    return 'removed'
   }
 }
