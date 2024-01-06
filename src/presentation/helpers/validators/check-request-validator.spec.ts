7be2b38f9ba00f7b384d1e409bc552bcc81c0d20
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-01-05 21:10:07 -0300

    test: ensure CheckRequestValidator return UnnecessaryParamsError if validation fails

---
+++ src/presentation/helpers/validators/check-request-validator.spec.ts
@@ -0,0 +1,19 @@
+import { UnnecessaryParamsError } from '../../errors/unecessary-params-error';
+import { HttpRequest } from '../../protocols/http';
+import { CheckRequestValidator } from './check-request-validator';
+
+const makeFakeRequest = (): HttpRequest => ({
+  body : {
+    any_field: 'any_field',
+    random_field: 'random_field'
+  }
+})
+
+const makeSut = (paramsValidate: string[]) => new CheckRequestValidator(paramsValidate)
+describe('CheckRequestValidator', () => {
+  test('should return UnnecessaryParamsError if validation fails', () => {
+    const sut = makeSut(['any_field'])
+    const response = sut.validation(makeFakeRequest())
+    expect(response).toEqual(new UnnecessaryParamsError())
+  })
+})
