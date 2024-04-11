32ca74fad7066e800c1e4bf3bf52707172308979
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-04-11 06:58:14 -0300

    feat: ensure UpdateStudentController call Validation with correct values

---
+++ src/presentation/controllers/student/update/update-student-controller.ts
@@ -0,0 +1,16 @@
+import { badRequest, ok } from '../../../helpers/http/http';
+import { Controller } from '../../../protocols/controller';
+import { HttpRequest, HttpResponse } from '../../../protocols/http';
+import { Validation } from '../../../protocols/validation';
+
+export class UpdateStudentController implements Controller {
+  constructor(private readonly validator: Validation){}
+
+  async handle (request: HttpRequest): Promise<HttpResponse> {
+    const error = this.validator.validation(request)
+    if(error) {
+      return badRequest(error)
+    }
+    return ok('success')
+  }
+}
