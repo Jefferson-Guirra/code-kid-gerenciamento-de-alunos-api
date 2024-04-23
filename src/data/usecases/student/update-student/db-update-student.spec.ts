75678168e065bfe4485337b4ba568494dffe93f6
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-04-22 22:12:42 -0300

    test: ensure DbUpdateStudent return throw updated student on succeeds

--- src/data/usecases/student/update-student/db-update-student.spec.ts
+++ src/data/usecases/student/update-student/db-update-student.spec.ts
@@ -13,22 +13,24 @@ const makeFakeRequest = () => ({
 
 })
 
+const updateStudentStub = (): AddStudentModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  age: 0,
+  father: 'any_father',
+  mother:'any_mother',
+  phone: 123456789,
+  course:['any_course'],
+  payment: 'any_payment',
+  date_payment: ['any_date'],
+  email: 'any_email@mail.com',
+  registration: 'active'
+})
+
 const makeUpdateStudentByIdStub = (): UpdateStudentByIdRepository => {
   class updateStudentByIdRepositoryStub implements UpdateStudentByIdRepository {
     async updateStudent (id: string, updateFields: any): Promise<AddStudentModel | null> {
-      return await Promise.resolve({
-        id: 'any_id',
-        name: 'any_name',
-        age: 0,
-        father: 'any_father',
-        mother:'any_mother',
-        phone: 123456789,
-        course:['any_course'],
-        payment: 'any_payment',
-        date_payment: ['any_date'],
-        email: 'any_email@mail.com',
-        registration: 'active'
-      }) 
+      return await Promise.resolve(updateStudentStub()) 
     }
   }
   return new updateStudentByIdRepositoryStub()
@@ -46,16 +48,24 @@ const makeSut = (): SutTypes =>  {
 }
 
 describe('DbUpdateStudent', () => {
+
   test('should UpdateStudentByIdRepository call UpdateStudentByIdRepository with correct values', async () => { 
     const { sut, updateStudentByIdStub } = makeSut()
     const updateSpy = jest.spyOn(updateStudentByIdStub, 'updateStudent')
     await sut.update('any_id', makeFakeRequest())
     expect(updateSpy).toHaveBeenCalledWith('any_id', makeFakeRequest())
   })
+
   test('should return throw if UpdateStudentByIdRepository fails ', async () => {
     const { sut, updateStudentByIdStub } = makeSut()
     jest.spyOn(updateStudentByIdStub, 'updateStudent').mockReturnValueOnce(Promise.reject(new Error()))
     const response = sut.update('any_id',makeFakeRequest())
     await expect(response).rejects.toThrow()
+  })
+
+  test('should return UpdateStudent on succeeds', async () => {
+    const {sut} = makeSut()
+    const response = await sut.update('any_id',makeFakeRequest())
+    expect(response).toEqual(updateStudentStub())
   })
 })
