4e9c6e6afb3424dd6d097b7216b2cf84455a1fdd
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-05-03 22:43:05 -0300

    feat: ensure update-student route return 401 if student not exist

--- src/main/routes/student-routes.ts
+++ src/main/routes/student-routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import { adapterRouter } from '../adapters/express/express-adapter-routes';
 import { makeAddStudentControllerFactory } from '../factories/student/add/add-student-controller-factory';
 import { makeRemoveStudentControllerFactory } from '../factories/student/remove/remove-student-controller-factory';
+import { makeUpdateStudentControllerFactory } from '../factories/student/update/update-student-controller-factory';
 
 const studentRoutes = (router: Router) => {
   router.post('/add-student', adapterRouter(makeAddStudentControllerFactory()))
   router.delete('/remove-student', adapterRouter(makeRemoveStudentControllerFactory()))
+  router.put('/update-student', adapterRouter(makeUpdateStudentControllerFactory()))
 }
 
 export default studentRoutes
