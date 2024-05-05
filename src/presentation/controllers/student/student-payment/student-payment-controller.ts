f2c0c576e9266c02dd8fb1570e13be44a0e25d4e
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-05-05 20:18:54 -0300

    feat: ensure StudentPaymentController return 400 if Validator with return a error

--- src/presentation/controllers/student/student-payment/student-payment-controller.ts
+++ src/presentation/controllers/student/student-payment/student-payment-controller.ts
@@ -1,4 +1,4 @@
-import { ok } from '../../../helpers/http/http';
+import { badRequest, ok } from '../../../helpers/http/http';
 import { Controller } from '../../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../../protocols/http';
 import { Validation } from '../../../protocols/validation';
@@ -7,6 +7,9 @@ export class StudentPaymentController implements Controller {
   constructor (private readonly validator: Validation){}
   async handle(request: HttpRequest): Promise<HttpResponse> {
     const error = this.validator.validation(request)
+    if(error) {
+      return badRequest(error)
+    }
     return ok('success')
   }
 }
