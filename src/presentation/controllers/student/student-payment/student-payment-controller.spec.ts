1ef9813e88e8b074b505a94341fc65e371291be1
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-05-06 21:58:52 -0300

    test: ensure StudentPaymentController return 500 if PaymentStudents return throw

--- src/presentation/controllers/student/student-payment/student-payment-controller.spec.ts
+++ src/presentation/controllers/student/student-payment/student-payment-controller.spec.ts
@@ -1,7 +1,7 @@
 import { AddStudentModel } from '../../../../domain/usecases/student/add-student';
 import { PaymentStudents } from '../../../../domain/usecases/student/payment-student';
 import { MissingParamsError } from '../../../errors/missing-params-error';
-import { badRequest, unauthorized } from '../../../helpers/http/http';
+import { badRequest, serverError, unauthorized } from '../../../helpers/http/http';
 import { HttpRequest } from '../../../protocols/http';
 import { Validation } from '../../../protocols/validation';
 import { StudentPaymentController } from './student-payment-controller';
@@ -90,5 +90,12 @@ describe('StudentPaymentController', () => {
     jest.spyOn(getPaymentStudentsStub, 'getStudents').mockReturnValueOnce(Promise.resolve(null))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(unauthorized())
+  })
+
+  test('should return 500 if PaymentStudents return throw ', async () => {
+    const { sut, getPaymentStudentsStub } =makeSut()
+    jest.spyOn(getPaymentStudentsStub, 'getStudents').mockReturnValueOnce(Promise.reject(new Error('')))
+    const response = await sut.handle(makeFakeRequest())
+    expect(response).toEqual(serverError(new Error()))
   })
 })
