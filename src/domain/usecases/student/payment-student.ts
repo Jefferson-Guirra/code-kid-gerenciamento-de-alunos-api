0aa652fe18738a05ffaffff20b0a59a7140b445f
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-05-06 21:48:07 -0300

    feat: ensure StudentPaymentController call PaymentStudents with correct value

---
+++ src/domain/usecases/student/payment-student.ts
@@ -0,0 +1,5 @@
+import { AddStudentModel } from './add-student';
+
+export interface PaymentStudents {
+  getStudents: (payment: string) => Promise<AddStudentModel[]| null>
+}
--- src/presentation/controllers/student/student-payment/student-payment-controller.ts
+++ src/presentation/controllers/student/student-payment/student-payment-controller.ts
@@ -1,15 +1,21 @@
+import { PaymentStudents } from '../../../../domain/usecases/student/payment-student';
 import { badRequest, ok } from '../../../helpers/http/http';
 import { Controller } from '../../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../../protocols/http';
 import { Validation } from '../../../protocols/validation';
 
 export class StudentPaymentController implements Controller {
-  constructor (private readonly validator: Validation){}
+  constructor (
+    private readonly validator: Validation,
+    private readonly paymentStudents: PaymentStudents
+  ){}
   async handle(request: HttpRequest): Promise<HttpResponse> {
+    const { payment } = request.body
     const error = this.validator.validation(request)
     if(error) {
       return badRequest(error)
     }
+    await this.paymentStudents.getStudents(payment)
     return ok('success')
   }
 }
