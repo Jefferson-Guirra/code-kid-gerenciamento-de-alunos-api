e063d527f159dd111525e1d896df1bdf5217d204
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-05-06 22:03:12 -0300

    test: ensure StudentPaymentController return 200 on succeeds

--- src/presentation/controllers/student/student-payment/student-payment-controller.spec.ts
+++ src/presentation/controllers/student/student-payment/student-payment-controller.spec.ts
@@ -1,7 +1,7 @@
 import { AddStudentModel } from '../../../../domain/usecases/student/add-student';
 import { PaymentStudents } from '../../../../domain/usecases/student/payment-student';
 import { MissingParamsError } from '../../../errors/missing-params-error';
-import { badRequest, serverError, unauthorized } from '../../../helpers/http/http';
+import { badRequest, ok, serverError, unauthorized } from '../../../helpers/http/http';
 import { HttpRequest } from '../../../protocols/http';
 import { Validation } from '../../../protocols/validation';
 import { StudentPaymentController } from './student-payment-controller';
@@ -97,5 +97,11 @@ describe('StudentPaymentController', () => {
     jest.spyOn(getPaymentStudentsStub, 'getStudents').mockReturnValueOnce(Promise.reject(new Error('')))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(serverError(new Error()))
+  })
+
+  test('should return 200 on succeeds', async () => {
+    const { sut } =makeSut()
+    const response = await sut.handle(makeFakeRequest())
+    expect(response).toEqual(ok([makeFakeStudent(), makeFakeStudent()]))
   })
 })
