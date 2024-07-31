503fd4d01596781b48b3476b64ba7a610ec483e5
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-07-31 20:55:22 -0300

    test: ensure makeGetStudentPaymentValidator call ValidatorComposite with correct values

---
+++ src/main/factories/student/get-payment-students/get-payment-students-validator-factory.spec.ts
@@ -0,0 +1,17 @@
+import { CheckRequestValidator } from '../../../../presentation/helpers/validators/check-request-validator';
+import { RequiredFieldsValidator } from '../../../../presentation/helpers/validators/required-fields-validator';
+import { ValidatorComposite } from '../../../../presentation/helpers/validators/validator-composite';
+import { Validation } from '../../../../presentation/protocols/validation';
+import { makeGetStudentPaymentValidator } from './get-payment-students-validator-factory';
+
+jest.mock('../../../../presentation/helpers/validators/validator-composite')
+
+
+describe('ValidatorComposite', () => { 
+  test('should call ValidatorComposite with correct values', () => { 
+    const validators: Validation[] = []
+    validators.push(new RequiredFieldsValidator('payment'), new CheckRequestValidator(['payment']))
+    makeGetStudentPaymentValidator()
+    expect(ValidatorComposite).toHaveBeenCalledWith(validators)
+  })
+})
