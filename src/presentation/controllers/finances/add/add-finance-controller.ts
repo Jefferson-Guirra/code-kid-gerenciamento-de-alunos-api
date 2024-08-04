85c7cfd7bde55e68fe1e9216c3d219642dc05db3
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-08-04 17:19:54 -0300

    feat: ensure AddFinanceController call Validatorwith correct values

--- src/presentation/controllers/finances/add/add-finance-controller.ts
+++ src/presentation/controllers/finances/add/add-finance-controller.ts
@@ -2,12 +2,15 @@ import { AddFinance } from '../../../../domain/usecases/finance/add-finance';
 import { ok } from '../../../helpers/http/http';
 import { Controller } from '../../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../../protocols/http';
+import { Validation } from '../../../protocols/validation';
 
 export class AddFinanceController implements Controller {
   constructor (
+    private readonly validator: Validation,
     private readonly finance: AddFinance
   ) {}
   async handle (request: HttpRequest): Promise<HttpResponse> {
+    const error = this.validator.validation(request)
     const body = request.body
     await this.finance.addFinance(body)
     return ok('ok')
