a333dd647462c7f216af5a2763ac072a5583d594
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-08-04 19:42:52 -0300

    refactor: add AccountRepository dependency in DbAddStudent

--- src/main/factories/student/add/add-student-controller-factory.ts
+++ src/main/factories/student/add/add-student-controller-factory.ts
@@ -1,12 +1,14 @@
 import { DbAddStudentRepository } from '../../../../data/usecases/student/add-student/db-add-student';
+import { AccountMongoRepository } from '../../../../infra/db/account/account-mongo-repository';
 import { StudentMongoRepository } from '../../../../infra/db/student/student-mongo-repository';
 import { AddStudentController } from '../../../../presentation/controllers/student/add/add-student-controller';
 import { Controller } from '../../../../presentation/protocols/controller';
 import { makeAddStudentValidatorFactory } from './add-student-validator-factory';
 
 export const makeAddStudentControllerFactory = (): Controller => {
+  const dbAccountRepository = new AccountMongoRepository()
   const validator = makeAddStudentValidatorFactory()
   const studentMongoRepository = new StudentMongoRepository()
-  const dbAddStudent = new DbAddStudentRepository(studentMongoRepository, studentMongoRepository)
+  const dbAddStudent = new DbAddStudentRepository( dbAccountRepository,studentMongoRepository, studentMongoRepository)
   return new AddStudentController(validator, dbAddStudent)
 }
