4ce4c939acd63c85013d07174050ecc5ff69fdbf
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-08-05 22:45:35 -0300

    feat: ensure makeRemoveStudentValidatorFactory call ValidatorComposite with correct values

--- src/main/factories/student/remove/remove-student-validator-factory.ts
+++ src/main/factories/student/remove/remove-student-validator-factory.ts
@@ -1,3 +1,4 @@
+import { CheckRequestValidator } from '../../../../presentation/helpers/validators/check-request-validator';
 import { RequiredFieldsValidator } from '../../../../presentation/helpers/validators/required-fields-validator';
 import { ValidatorComposite } from '../../../../presentation/helpers/validators/validator-composite';
 import { Validation } from '../../../../presentation/protocols/validation';
@@ -5,5 +6,6 @@ import { Validation } from '../../../../presentation/protocols/validation';
 export const makeRemoveStudentValidatorFactory = (): Validation => {
   const validators: Validation[] = []
   validators.push(new RequiredFieldsValidator('id'))
+  validators.push(new CheckRequestValidator(['id', 'accessToken']))
   return new ValidatorComposite(validators) 
 }
