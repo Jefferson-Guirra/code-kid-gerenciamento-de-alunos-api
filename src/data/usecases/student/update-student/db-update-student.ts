e01f5b42be6bbcb26d35d5bea29ddf66a0133674
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-08-05 23:23:31 -0300

    feat: ensure DbUpdateStudent call LoadAccountByAccessTokenRepository with correct value

--- src/data/usecases/student/update-student/db-update-student.ts
+++ src/data/usecases/student/update-student/db-update-student.ts
@@ -1,10 +1,16 @@
 import { AddStudentModel } from '../../../../domain/usecases/student/add-student';
 import { UpdateStudent } from '../../../../domain/usecases/student/update-student';
+import { LoadAccountByAccessTokenRepository } from '../../../protocols/db/account/load-account-by-access-token-repository';
 import { UpdateStudentByIdRepository } from '../../../protocols/db/student/update-student-by-id-repository';
 
 export class DbUpdateStudent implements UpdateStudent {
-  constructor( private readonly updateStudentByIdRepository: UpdateStudentByIdRepository) {}
+  constructor( 
+    private readonly loadAccount: LoadAccountByAccessTokenRepository,
+    private readonly updateStudentByIdRepository: UpdateStudentByIdRepository
+  ) {}
   async update (id: string, updateFields: any): Promise<AddStudentModel | null> {
-    return await this.updateStudentByIdRepository.updateStudent(id, updateFields)
+    const {accessToken, ...fields} = updateFields 
+    this.loadAccount.loadByAccessToken(accessToken)
+    return await this.updateStudentByIdRepository.updateStudent(id, fields)
   }
 }
