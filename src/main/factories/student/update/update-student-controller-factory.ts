e6ac7c19b07b34945903b8011f19a004c8cf27de
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-08-05 23:41:14 -0300

    feat: add new AccountMongoRepository dependency in dbUpdateStudent

--- src/main/factories/student/update/update-student-controller-factory.ts
+++ src/main/factories/student/update/update-student-controller-factory.ts
@@ -1,13 +1,15 @@
 import { DbUpdateStudent } from '../../../../data/usecases/student/update-student/db-update-student';
+import { AccountMongoRepository } from '../../../../infra/db/account/account-mongo-repository';
 import { StudentMongoRepository } from '../../../../infra/db/student/student-mongo-repository';
 import { UpdateStudentController } from '../../../../presentation/controllers/student/update/update-student-controller';
 import { Controller } from '../../../../presentation/protocols/controller';
 import { makeValidatorUpdateStudentFactory } from './update-student-validator-factory';
 
 export const makeUpdateStudentControllerFactory = (): Controller => {
+  const accountMongoRepository = new AccountMongoRepository()
   const updateValidatorComposite = makeValidatorUpdateStudentFactory()
   const studentMongoRepository = new StudentMongoRepository()
-  const dbUpdateStudent = new DbUpdateStudent(studentMongoRepository)
+  const dbUpdateStudent = new DbUpdateStudent( accountMongoRepository,studentMongoRepository)
   const updateStudentController = new UpdateStudentController(updateValidatorComposite,dbUpdateStudent)
   return updateStudentController
 }
