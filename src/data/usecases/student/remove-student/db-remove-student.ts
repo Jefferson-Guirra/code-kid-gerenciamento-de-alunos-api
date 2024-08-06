a42465aeae98fb68522846e1a0c4a242b8b8f660
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-08-05 22:21:14 -0300

    feat: ensure DbRemoveStudent call loadAccount with correct value

--- src/data/usecases/student/remove-student/db-remove-student.ts
+++ src/data/usecases/student/remove-student/db-remove-student.ts
@@ -1,13 +1,16 @@
 import { RemoveStudent } from '../../../../domain/usecases/student/remove-student';
+import { LoadAccountByAccessTokenRepository } from '../../../protocols/db/account/load-account-by-access-token-repository';
 import { LoadStudentByIdRepository } from '../../../protocols/db/student/load-student-by-id-repository';
 import { RemoveStudentByIdRepository } from '../../../protocols/db/student/remove-student-by-id-repository';
 
 export class DbRemoveStudent implements RemoveStudent {
   constructor(
+    private readonly loadAccount: LoadAccountByAccessTokenRepository,
     private readonly loadStudent: LoadStudentByIdRepository,
     private readonly removeStudent: RemoveStudentByIdRepository
     ) {}
-  async remove(id: string): Promise<'removed' | null> {
+  async remove(accessToken: string ,id: string): Promise<'removed' | null> {
+    await this.loadAccount.loadByAccessToken(accessToken)
     const student = await this.loadStudent.loadById(id)
     if(!student) {
       return null
