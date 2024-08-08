ead3a3efdf9396c59c855e216389072eb7047945
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-08-08 06:41:25 -0300

    feat: ensure DbGetPaymentStudents call LoadAccount with correct value

--- src/data/usecases/student/payment/get-payment-students/db-get-payment-students.ts
+++ src/data/usecases/student/payment/get-payment-students/db-get-payment-students.ts
@@ -1,15 +1,18 @@
 import { AddStudentModel } from '../../../../../domain/usecases/student/add-student';
 import { PaymentStudents, UserGetPayment } from '../../../../../domain/usecases/student/payment-student';
+import { LoadAccountByAccessTokenRepository } from '../../../../protocols/db/account/load-account-by-access-token-repository';
 import { getPaymentStudentsRepository } from '../../../../protocols/db/student/get-payment-students-repository';
 
 export class DbGetPaymentStudents implements PaymentStudents {
   constructor(
+    private readonly loadAccount: LoadAccountByAccessTokenRepository,
     private readonly paymentStudents: getPaymentStudentsRepository
   ) {
 
   }
-  async getStudents (getPayment: UserGetPayment): Promise<AddStudentModel[] | null> {
-    const { accessToken, payment} = getPayment
+  async getStudents (request: UserGetPayment): Promise<AddStudentModel[] | null> {
+    const { accessToken, payment} = request
+      await this.loadAccount.loadByAccessToken(accessToken)
     return await this.paymentStudents.getPaymentStudents(payment)
   }
 }
