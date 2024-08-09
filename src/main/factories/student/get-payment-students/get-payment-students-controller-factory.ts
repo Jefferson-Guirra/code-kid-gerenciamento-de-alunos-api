7893a1ebf842515a9b9b56d99faf7d8f574b03f9
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-08-09 06:31:35 -0300

    feat: add new accountMongoRepository dependency in DbGetPaymentStudents

--- src/main/factories/student/get-payment-students/get-payment-students-controller-factory.ts
+++ src/main/factories/student/get-payment-students/get-payment-students-controller-factory.ts
@@ -1,12 +1,14 @@
 import { DbGetPaymentStudents } from '../../../../data/usecases/student/payment/get-payment-students/db-get-payment-students';
+import { AccountMongoRepository } from '../../../../infra/db/account/account-mongo-repository';
 import { StudentMongoRepository } from '../../../../infra/db/student/student-mongo-repository';
 import { StudentPaymentController } from '../../../../presentation/controllers/student/student-payment/student-payment-controller';
 import { Controller } from '../../../../presentation/protocols/controller';
 import { makeGetStudentPaymentValidator } from './get-payment-students-validator-factory';
 
 export const makeGetPaymentStudentsControllerFactory = (): Controller => {
+  const accountMongoRepository = new AccountMongoRepository()
   const validator = makeGetStudentPaymentValidator()
   const studentMongoRepository = new StudentMongoRepository()
-  const dbGetPaymentController = new DbGetPaymentStudents(studentMongoRepository)
+  const dbGetPaymentController = new DbGetPaymentStudents( accountMongoRepository,studentMongoRepository)
   return new StudentPaymentController(validator, dbGetPaymentController)
 } 
