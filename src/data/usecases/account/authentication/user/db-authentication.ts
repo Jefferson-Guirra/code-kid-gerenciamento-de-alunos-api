71a5f24f806ce77f132744321094d04c280702d8
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-08-12 21:20:49 -0300

    feat: ensure DbAuthentication call UpdateAccessTokenRepository with correct value

--- src/data/usecases/account/authentication/user/db-authentication.ts
+++ src/data/usecases/account/authentication/user/db-authentication.ts
@@ -2,30 +2,36 @@ import { Authentication, AuthenticationModel } from '../../../../../domain/useca
 import { Encrypter } from '../../../../protocols/criptography/encrypter';
 import { HashCompare } from '../../../../protocols/criptography/hash-compare';
 import { LoadAccountByEmailRepository } from '../../../../protocols/db/account/load-account-by-email-repository';
+import { UpdateAccessTokenRepository } from '../../../../protocols/db/account/update-access-token-repository';
 
 export class DbAuthentication implements Authentication {
   constructor(  
     private readonly loadAccount: LoadAccountByEmailRepository,
     private readonly hashCompare: HashCompare,
-    private readonly encrypter: Encrypter
+    private readonly encrypter: Encrypter,
+    private readonly updateAccessToken: UpdateAccessTokenRepository
     
     ) {}
   async auth (email: string, password: string): Promise<AuthenticationModel | null> {
     const account = await this.loadAccount.loadByEmail(email)
-    if(!account) {
-      return null
-    }
-    const isValid = await this.hashCompare.compare(password, account.password)
-    if(!isValid) {
-      return null
-    }
-    const accessToken = await this.encrypter.encrypt(account.id)
 
-  return {
-    email: account.email,
-    username: account.username,
-    accessToken
+    if (account) {
+      const { username, email, id} = account
+      const isValid = await this.hashCompare.compare(password, account.password)
+      
+      if(isValid) {
+        const accessToken = await this.encrypter.encrypt(account.id)
+        const updatedToken = await this.updateAccessToken.update(id, accessToken)
 
-  }
+        if(!updatedToken) {
+          return null
+        }
+
+        return  await Promise.resolve({email, username, accessToken})
+
+      }
+    }
+
+    return null
   }
 }
